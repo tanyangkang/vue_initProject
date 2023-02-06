539ccf4c7e81e724e1d0cedb252023b07e9a01bc
谭阳康 <tanyangkang@tiduyun.com>
2023-02-06 14:07:13 +0800

    feat: Business

---
+++ src/components/Business.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Business
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
