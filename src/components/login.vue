e7f7df8cde96cfe59dcc13f7b12165a11094aa4d
谭阳康 <tanyangkang@tiduyun.com>
2023-02-06 10:58:02 +0800

    feat: login

---
+++ src/components/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <span>登陆</span>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
